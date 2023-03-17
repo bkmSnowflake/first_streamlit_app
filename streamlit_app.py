eadfcee70eb796fec91566bf9fa4be74bd0971c6
bkmSnowflake <126919320+bkmSnowflake@users.noreply.github.com>
2023-03-17 18:15:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
-
-streamlit.title('My Mom\'s New Healthy Diner')
-
+import pandas
+import requests
+import snowflake.connector
 from urllib.error import URLError
 
+streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
